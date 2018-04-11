498293462915cf45486b198318adb49e86e535ca
frander170896 <franderramirez@yahoo.es>
2018-04-11 15:32:21 -0600

    elementos del search job

---
+++ Proyecto-React/src/Componentes/Search_Job/Lateral_Filter.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class InputDate extends Component{
+    render(){
+        return (
+            <div>
+                <label className=" col-form-label">{this.props.Lnombre}</label>
+                <div className="form-group">
+                    <input className="form-control" type="date"/>
+                </div>
+            </div>
+        );
+    }
+}
+
+class CheckBox extends Component{
+    render(){
+        return (
+          
+        );
+    }
+}
+
+
+class LateralFilter extends Component{
+
+    render(){
+        return(
+           <form>
+               <InputDate Lnombre="Fecha Publicación"></InputDate>
+           </form>
+        );
+    }
+
+}
+
+export default LateralFilter;
--- Proyecto-React/src/Componentes/Search_Job/index.js
+++ Proyecto-React/src/Componentes/Search_Job/index.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-
+import LateralFilter from './Lateral_Filter';
 
     class Search extends Component {
         constructor(props){
             super(props);  
         }
+
         render(){
             return (
-                <h1>Hola</h1>
+                <div className="container-fluid">
+                    <div className="row search">
+                        <LateralFilter></LateralFilter>
+                    </div>
+                    <div className="row jobs">
+                        <div className="col-ms-12 col-md-4 col-lg-4" >
+
+                        </div>
+                    </div>
+                </div>
             );
         }
     }
-
 
 
 export default Search;
