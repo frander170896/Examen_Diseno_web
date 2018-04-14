9e570ad6455c5e9f53d1aa0fdf44949b67d8260f
frander170896 <franderramirez@yahoo.es>
2018-04-14 17:40:02 -0600

    consumo de web service

--- Proyecto-React/src/Componentes/Search_Job/Jobs.js
+++ Proyecto-React/src/Componentes/Search_Job/Jobs.js
@@ -1,21 +1,19 @@
 import axios from 'axios';
 
-export function get_Jobs() {
-    var datos = [];
-    axios.get('https://jobs.github.com/positions.json')
+    const get_Jobs =  (metodo,referencia) => {
+    var uri = window.encodeURI('https://jobs.github.com/positions.json');
+    axios.get(uri)
         .then(function (response) {
-            datos = response.data;
-            /*console.log(datos); //Datos de la respuesta
+            
+            console.log(response.data); //Datos de la respuesta
             console.log(response.status); //Estatus
             console.log(response.statusText); //Mensaje del estatus
-            console.log(response.headers); // Encabezados*/
-        })
-        .catch(function (error) {
-            /*console.log(error.response); //Objeto respuesta
-            console.log(error.response.data); // Respuesta del servidor
-            console.log(error.response.status); //Código de error
-            console.log(error.response.statusText); //Mensaje del error*/
-        });
+            console.log(response.headers); // Encabezados
+            alert("hola"+response.data);
+            metodo(response.data,referencia);
+         
 
-    return datos;
-}
+        });
+ } 
+
+export default get_Jobs;
