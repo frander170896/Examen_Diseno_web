751fd735b81227e052ead3747057092579efb108
gersonvargas <gersonvargasgalvez@gmail.com>
2018-04-15 13:30:42 -0600

    Adding about us and HOME
    Home not finished yet, I am working on

--- Proyecto-React/src/Componentes/About/index.js
+++ Proyecto-React/src/Componentes/About/index.js
@@ -3,22 +3,25 @@ import React, { Component } from 'react';
 import about_us from "../Global/images/About.jpg";
 import '../Global/css/About/about.css';
 
+import CardGroup from './CardGroup';
 class About extends Component {
     render() {
         return (
-                <div className="About">
-                    <div className='row'>
-                        <div className='col-md-6'>
-                            <img src={about_us} className="img-fluid" alt="about us" id=""/>
-                        </div>
-                        <div className='col-md-6'>
-                            <h1>About us</h1>
-                            <p>Founded in April of 2018, job search by Delta is trusted web site to search jobs from GitHub from anyplace of the world.</p>
-                            <p>Job search by Delta connects people to jobs and it is the easiest way for this people to search this information by differents ways, for example, by categories.</p>
-                </div>
+            <div className="About">
+                <div className='row'>
+                
+                    <div className='col-md-6'>
+                        <img src={about_us} className="img-fluid" alt="about us" id="" />
                     </div>
+                    <div className='col-md-6'>
+                        <h1>About us</h1>
+                        <p>Founded in April of 2018, job search by Delta is trusted web site to search jobs from GitHub from anyplace of the world.</p>
+                        <p>Job search by Delta connects people to jobs and it is the easiest way for this people to search this information by differents ways, for example, by categories.</p>
+                    </div>
+                    <CardGroup/>
                 </div>
-                );
+            </div>
+        );
     }
 }
 
--- Proyecto-React/src/Componentes/Home/index.js
+++ Proyecto-React/src/Componentes/Home/index.js
@@ -1,11 +1,14 @@
 // Dependencies
 import React, { Component } from 'react';
+import CardGroup from './CardGroup';
+
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <h1>Home</h1>
+        <h1>Are you looking for a job? <i>This is the right place to start...</i></h1>
+        <CardGroup/>
       </div>
     );
   }
