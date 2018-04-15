ce6cb7ea07e78fec9730435827ec478357b52e9b
marco <marco@DESKTOP-O9DPP57>
2018-04-15 07:14:23 -0600

    form completed and font awesome added to project

--- Proyecto-React/src/Componentes/Contact/index.js
+++ Proyecto-React/src/Componentes/Contact/index.js
@@ -2,36 +2,53 @@
 import React, { Component }
 from 'react';
 
+
+
 class Contact extends Component {
-render() {
-return (
-<div className="row">
-    <div className="col-md-3"></div>
-    <div class="card border-dark col-md-6">
-        <div class="card-header h2">Contact</div>
-        <div class="card-body text-dark">
-            <p class="card-text">Thank you for using this product. Please, complete the form below, so we can provide quick and efficient service.</p>
-            <form>
-            <div class="form-group">
-                <label class="sr-only" for="name">Name</label>
-                <div class="input-group mb-2">
-                    <div class="input-group-prepend">
-                        <div class="input-group-text">@</div>
+    render() {
+        return (
+                <div className="row">
+                    <div className="col-md-3"></div>
+                    <div className="card border-dark col-md-6">
+                        <div className="card-header h2">Contact</div>
+                        <div className="card-body text-dark">
+                            <p className="card-text">Thank you for using this product. Please, complete the form below, so we can provide quick and efficient service.</p>
+                            <form>
+                                <div class="form-group">
+                                    <label className="sr-only" for="name">Name</label>
+                                    <div className="input-group mb-2">
+                                        <div className="input-group-prepend">
+                                            <div className="input-group-text fa fa-user"></div>
+                                        </div>
+                                        <input type="text" class="form-control" id="name" placeholder="Name"/>
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <label className="sr-only" for="phone">Phone</label>
+                                    <div className="input-group mb-2">
+                                        <div className="input-group-prepend">
+                                            <div className="input-group-text fa fa-phone"></div>
+                                        </div>
+                                        <input type="text" className="form-control" id="phone" placeholder="Phone"/>
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <label className="sr-only" for="email">Email</label>
+                                    <div className="input-group mb-2">
+                                        <div className="input-group-prepend">
+                                            <div className="input-group-text">@</div>
+                                        </div>
+                                        <input type="email" className="form-control" id="email" placeholder="Email"/>
+                                    </div>
+                                </div>
+                                <button type="submit" className="btn btn-primary mb-2">Send</button>
+                            </form>
+                        </div>
                     </div>
-                    <input type="text" class="form-control" id="name" placeholder="Name"/>
+                    <div className="col-md-3"></div>
                 </div>
-                </div>
-                <div class="form-group">
-                    <label for="formGroupExampleInput2">Another label</label>
-                    <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input"/>
-                </div>
-            </form>
-        </div>
-    </div>
-    <div className="col-md-3"></div>
-</div>
-);
-}
+                );
+    }
 }
 
 export default Contact;
