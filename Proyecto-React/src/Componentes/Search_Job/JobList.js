fb74b3fa429d16940a06d525008972e054e10eb7
gersonvargas <gersonvargasgalvez@gmail.com>
2018-04-15 11:28:03 -0600

    job detail and job list modal

--- Proyecto-React/src/Componentes/Search_Job/JobList.js
+++ Proyecto-React/src/Componentes/Search_Job/JobList.js
@@ -4,18 +4,18 @@ import JobDetail from './JobDetail';
 class CompanyLogo extends Component {
     render() {
         return (
-                <img src={this.props.company_logo} className="img-thumbnail" alt="company" />
-                );
+            <img src={this.props.company_logo} className="img-thumbnail" alt="company" />
+        );
     }
 }
 
 class ApplyLink extends Component {
     render() {
         return (
-                <a className='active btn btn-link' href={this.props.company_url}>
-                    {this.props.action_name}
-                </a>
-                );
+            <a className='active btn btn-link' href={this.props.company_url}>
+                {this.props.action_name}
+            </a>
+        );
     }
 }
 class JobItem extends Component {
@@ -30,47 +30,51 @@ class JobItem extends Component {
             company_description: 'We are improving',
             created_at: null,
             how_to_apply: 'Email us!',
-            show:false
+            show: false
         };
+        this.handleShow=this.handleShow.bind(this);
     }
     handleShow() {
-    this.setState({ show: true });
-  }
+        this.setState({ show: true });
+
+        console.log('Handle show JOB LIST'+ this.state.show? 'True':'False');
+    }
     render() {
         return (
+            <div className="card border-success">
+               {this.state.show?<JobDetail visible={this.state.show} />:'JOB DETAIL WAITING'}
                 <div className="card border-success">
-        <JobDetail visible={this.state.show} />
-                    <div className="card border-success">
-                        <div className="card-body">
-                            <h2 className="card-title">{this.props.jobtitle}</h2>
-                            <h3 className="card-subtitle mb-2 text-muted">About the job</h3>
-                            <p>
-                                <i><strong>Job Type:  </strong> {this.props.job_type}</i>
-                
-                            </p>
-                            <p><i><strong>Created:</strong>  {this.props.created_at}</i> </p>
-                            <p className="card-text">{this.props.job_description}</p>
-                            <p>Click to get the full Modal experience!</p>
-                
-                
-                            <button className="btn btn-primary" onClick={this.handleShow}>View Job and apply</button>
-                            <p>
-                                <i><strong>How to apply: </strong>{this.props.how_to_apply}</i>
+                    <div className="card-body">
+                        <h2 className="card-title">{this.props.jobtitle}</h2>
+                        <h3 className="card-subtitle mb-2 text-muted">About the job</h3>
+                        <p>
+                            <i><strong>Job Type:  </strong> {this.props.job_type}</i>
+
+                        </p>
+                        <p><i><strong>Created:</strong>  {this.props.created_at}</i> </p>
+                        <p className="card-text">{this.props.job_description}</p>
+                        <p>Click to get the full Modal experience!</p>
+
+
+                        <button className="btn btn-primary" onClick={this.handleShow}>View Job and apply</button>
+
+                        <p>
+                            <i><strong>How to apply: </strong>{this.props.how_to_apply}</i>
                             <ApplyLink company_url={this.props.company_url} action_name={'Go to companys page...'} />
-                            </p>
-                            <h3 className="card-subtitle mb-2 text-muted">About the company</h3>
-                            <CompanyLogo company_logo={this.props.company_logo} />
-                            <p>
-                                <i>{this.props.company_description}
-                                    <ApplyLink company_url={this.props.company_url} action_name={'Visit Us'} />
-                                </i>
-                            </p>
-                            <div className="card-footer"></div>
-                        </div>
+                        </p>
+                        <h3 className="card-subtitle mb-2 text-muted">About the company</h3>
+                        <CompanyLogo company_logo={this.props.company_logo} />
+                        <p>
+                            <i>{this.props.company_description}
+                                <ApplyLink company_url={this.props.company_url} action_name={'Visit Us'} />
+                            </i>
+                        </p>
+                        <div className="card-footer"></div>
                     </div>
-                    
                 </div>
-                );
+
+            </div>
+        );
     }
 
 }
@@ -84,18 +88,18 @@ class JobList extends Component {
     }
     render() {
         return (
-                this.state.jobs.map(elemento => <div key={elemento.id}>
-                    <JobItem jobtitle={elemento.title}
-                             job_type={elemento.type}
-                             job_description={elemento.description}
-                             created_at={elemento.created_at}
-                             how_to_apply={elemento.how_to_apply}
-                             company_logo={elemento.company_logo}
-                             company_url={elemento.company_url}
-                             />
-                </div>)
+            this.state.jobs.map(elemento => <div key={elemento.id}>
+                <JobItem jobtitle={elemento.title}
+                    job_type={elemento.type}
+                    job_description={elemento.description}
+                    created_at={elemento.created_at}
+                    how_to_apply={elemento.how_to_apply}
+                    company_logo={elemento.company_logo}
+                    company_url={elemento.company_url}
+                />
+            </div>)
 
-                )
+        )
     }
 
 }
