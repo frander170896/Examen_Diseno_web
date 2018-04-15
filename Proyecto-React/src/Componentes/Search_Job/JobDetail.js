336c2ada168c79bd6d6067cb7f35339409b81f5d
marcoc22 <mtcanalesmesen@gmail.com>
2018-04-15 14:20:59 -0600

    html string converted to real html, please, use npm install

--- Proyecto-React/src/Componentes/Search_Job/JobDetail.js
+++ Proyecto-React/src/Componentes/Search_Job/JobDetail.js
@@ -5,6 +5,7 @@ import Popover from 'react-bootstrap/lib/Popover';
 import Tooltip from 'react-bootstrap/lib/Tooltip';
 import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
 import Button from 'react-bootstrap/lib/Button';
+import renderHTML from 'react-render-html';
 
     class JobDetail extends Component {
    constructor(props, context) {
@@ -22,7 +23,7 @@ import Button from 'react-bootstrap/lib/Button';
           <h5 className="modal-title">Red Alert!</h5>
         </div>
         <div className="modal-body">
-          <p>{this.state.description}</p>
+          {renderHTML(this.state.description)}
         </div>
         <div className="modal-footer">
           <button type="button" class="btn btn-primary">Save changes</button>
