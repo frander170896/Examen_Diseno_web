b84b61035931b257be1ba7e4367dbf5fb73e40ca
marco <marco@DESKTOP-O9DPP57>
2018-04-15 07:35:21 -0600

    from about, the sass file include some properties that are not correct for the img of the page, it was removed.

--- Proyecto-React/src/Componentes/About/index.js
+++ Proyecto-React/src/Componentes/About/index.js
@@ -8,14 +8,13 @@ class About extends Component {
                 <div className="About">
                     <div className='row'>
                         <div className='col-md-6'>
-                            <img src={about_us} className="img-thumbnail imgheader" alt="about us" />
+                            <img src={about_us} className="img-fluid" alt="about us" id=""/>
                         </div>
                         <div className='col-md-6'>
                             <h1>About us</h1>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum, urna nec dapibus laoreet, nisi augue tristique nibh, ac sodales dolor leo quis lacus. Nullam sit amet imperdiet erat. Mauris gravida suscipit velit, ut faucibus velit dictum a. Proin tempor nibh lorem, eget commodo risus cursus sit amet. Proin eget luctus lacus. Nulla euismod, urna a scelerisque varius, nibh sapien laoreet neque, tristique posuere odio arcu eu leo. Integer dictum, enim ut pulvinar laoreet, justo ante imperdiet nisi, vel venenatis diam ipsum vitae odio. Nunc congue, diam eu placerat dapibus, tortor augue efficitur augue, ut luctus dolor nisi vitae eros. Maecenas ac aliquam ante, ac mollis orci. Sed tincidunt libero at iaculis egestas. Nunc lorem dolor, pulvinar eu ullamcorper non, viverra sit amet diam. Praesent vel elit sed eros mollis vehicula vel at ipsum. Nam a eros justo. Praesent non commodo purus.
-
-                                Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec viverra, ante a gravida iaculis, nulla urna interdum sem, ac laoreet felis justo quis est. </p>
-                        </div>
+                            <p>Founded in April of 2018, job search by Delta is trusted web site to search jobs from GitHub from anyplace of the world.</p>
+                            <p>Job search by Delta connects people to jobs and it is the easiest way for this people to search this information by differents ways, for example, by categories.</p>
+                </div>
                     </div>
                 </div>
                 );
