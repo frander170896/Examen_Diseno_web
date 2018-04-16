61cf739c00051cdabc6eb6a0542eefb220f2f952
frander170896 <franderramirez@yahoo.es>
2018-04-15 20:34:12 -0600

    filtrado de jobs

--- Proyecto-React/src/Componentes/Search_Job/Lateral_Filter.js
+++ Proyecto-React/src/Componentes/Search_Job/Lateral_Filter.js
@@ -14,7 +14,7 @@ class InputDate extends Component {
             <div>
                 <label className=" col-form-label">{this.props.Lnombre}</label>
                 <div className="form-group">
-                    <input className="form-control" onChange={(event) =>{ this.handleChange(event,1) }} type="date" />
+                    <input className="form-control" onChange={(event) =>{ this.handleChange(event,1) }} type="text" />
                 </div>
             </div>
         );
@@ -187,7 +187,7 @@ class SelectUbicacion extends Component {
                         </option>
                     )}
                 </select>
-                <label>Empresa:</label>
+                <label>Company Name:</label>
                 <select className="custom-select" onChange={(event) =>{ this.handleChange(event,3) }} >
                     {this.state.empresas ?
                         //  <SelectEmpresa Lnombre="Empresa" values={this.state.empresas}></SelectEmpresa>                  
@@ -228,15 +228,45 @@ class LateralFilter extends Component {
         super(props);
         this.state = {
             datos: this.props.job_list,
+            datosFiltrados:null,
             fecha:"",
-
+            ubicacion:"",
+            empresa :""
         }
         this.handleChange = this.handleChange.bind(this);
     }
+    filtrarSeleccion(array,valor){
+        var resultado = [];
+        var i;
+        for (i = 0; i < array.length; i++) {
+            if (array[i].include(valor)) {
+                resultado.push(array[i]);
+            }
+        }
+
+        return resultado;
+    }
+
     handlerSubmit(event){
         event.preventDefault();
+        var tipo1 = document.getElementById('Radio1');
+        var tipo2 = document.getElementById('Radio2');
+        var tipo = false;
+        if(tipo1.checked){
+            tipo = true;
+        }
+        var fecha = this.state.fecha;
+        var ubicacion =this.state.ubicacion;
+        var empresa = this.state.empresa;
+
+        var datos = this.state.datos;
         
-   
+        var resultado = this.filtrarSeleccion(datos,empresa);
+
+        this.setState(()=>{
+            datosFiltrados: resultado
+        });
+
     }
     handleChange(event,valor){
         
@@ -267,15 +297,15 @@ class LateralFilter extends Component {
     render() {
         return (
             <form onSubmit={this.handlerSubmit}>
-                <InputDate Lnombre="Fecha Publicación" mPadre={ this.handleChange } ></InputDate>
-                <SelectUbicacion mPadre={ this.handleChange } Lnombre="Ubicación" values={this.state.datos}></SelectUbicacion>
+                <InputDate Lnombre="Description" mPadre={ this.handleChange } ></InputDate>
+                <SelectUbicacion mPadre={ this.handleChange } Lnombre="Location" values={this.state.datos}></SelectUbicacion>
                 <div className="custom-control custom-radio">
-                    <input type="radio" id="customRadio1" name="customRadio" className="custom-control-input" />
-                    <label className="custom-control-label" for="customRadio1">Toggle this custom radio</label>
+                    <input type="radio" id="Radio1" value ="Full-Time" name="customRadio" className="custom-control-input" checked />
+                    <label className="custom-control-label" for="customRadio1">Full-Time</label>
                 </div>
                 <div className="custom-control custom-radio">
-                    <input type="radio" id="customRadio2" name="customRadio" className="custom-control-input" />
-                    <label className="custom-control-label" for="customRadio2">Or toggle this other custom radio</label>
+                    <input type="radio" id="Radio2" name="customRadio" className="custom-control-input" />
+                    <label className="custom-control-label" value="Middle-Time" for="customRadio2">Middle-Time</label>
                 </div>
                 <input type="submit" value="Filtrar" className="btn btn-primary"></input>
 
