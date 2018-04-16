49780f44abf08ab865d23c112bb5107fe01a90d9
gersonvargas <gersonvargasgalvez@gmail.com>
2018-04-16 12:11:19 -0600

    Job list no longer needed

--- Proyecto-React/src/Componentes/Search_Job/Lateral_Filter.js
+++ Proyecto-React/src/Componentes/Search_Job/Lateral_Filter.js
@@ -4,84 +4,10 @@ import axios from 'axios';
 import PropTypes from 'prop-types';
 import { render } from 'react-dom';
 import get_Jobs from './Jobs';
-import JobList from './JobList.js';
-import JobDetail from './JobDetail';
-
-class CompanyLogo extends Component {
-    render() {
-        return (
-            <img src={this.props.company_logo} className="img-thumbnail" alt="company" />
-        );
-    }
-}
-
-class ApplyLink extends Component {
-    render() {
-        return (
-            <a className='active btn btn-link' href={this.props.company_url}>
-                {this.props.action_name}
-            </a>
-        );
-    }
-}
-
-class JobItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            jobtitle: 'JOB TITLE',
-            job_description: 'JOB DESCRIPTION',
-            company_logo: "../Global/icons/reunion.png",
-            company_url: 'http://www.google.com',
-            job_type: 'Full time',
-            company_description: 'We are improving',
-            created_at: null,
-            how_to_apply: 'Email us!',
-            show: false
-        };
-        this.handleShow=this.handleShow.bind(this);
-    }
-    handleShow() {
-        this.setState({ show: true });
-    }
-    render() {
-        return (
-            <div className="card border-success">
-               {this.state.show?<JobDetail show={this.state.show}  visible={this.state.show} company_url={this.props.company_url} company_description={this.props.company_description} how_to_apply={this.props.how_to_apply} company_logo={this.props.company_logo} jobtitle={this.props.jobtitle} description={this.props.job_description}/>:''}
-                <div className="card border-success">
-                    <div className="card-body">
-                        <h2 className="card-title">{this.props.jobtitle}</h2>
-                        <h3 className="card-subtitle mb-2 text-muted">About the job</h3>
-                        <p>
-                            <i><strong>Job Type:  </strong> {this.props.job_type}</i>
-
-                        </p>
-                    
-                        <p className="card-text">{this.props.created_at}</p>
+import JobItem from './JobItem';
 
 
-                        <button className="btn btn-primary" onClick={this.handleShow}>View Job and apply</button>
 
-                        <p>
-                            <ApplyLink company_url={this.props.company_url} action_name={'Go to companys page...'} />
-                        </p>
-                        <h3 className="card-subtitle mb-2 text-muted">About the company</h3>
-                        <h4>{this.props.company}</h4>
-                        <CompanyLogo company_logo={this.props.company_logo} />
-                        <p>
-                            <i>{this.props.company_description}
-                                <ApplyLink company_url={this.props.company_url} action_name={'Visit Us'} />
-                            </i>
-                        </p>
-                        <div className="card-footer"></div>
-                    </div>
-                </div>
-
-            </div>
-        );
-    }
-
-}
 class InputDate extends Component {
     constructor(props) {
         super(props);
@@ -419,13 +345,24 @@ class LateralFilter extends Component {
                     </form>
                 </div>
                 <div className="col-ms-6 col-md-9 col-lg-9 mt-3" >
-                    <button onClick={this.handlerSubmit}> Click me</button>
+                <h5 className='m-3 b-2'>Available Jobs:</h5>
                     {!this.state.jobs && !this.state.isfiltrado ?
                         <div className="alert alert-primary" role="alert">
                             <i><strong>Loading...</strong></i>
                         </div>
                         : !this.state.isfiltrado && !this.state.datosFiltrados ?
-                            <JobList job_list={this.state.jobs} />
+                            this.state.jobs.map(elemento => <div key={elemento.id}>
+                                <JobItem jobtitle={elemento.title}
+                                    job_type={elemento.type}
+                                    job_description={elemento.description}
+                                    created_at={elemento.created_at}
+                                    how_to_apply={elemento.how_to_apply}
+                                    company_logo={elemento.company_logo}
+                                    company_url={elemento.company_url}
+                                    company={elemento.company}
+                                />
+                            </div>
+                            )
                             :
                             this.state.datosFiltrados.map(elemento => <div key={elemento.id}>
                                 <JobItem jobtitle={elemento.title}
@@ -439,7 +376,6 @@ class LateralFilter extends Component {
                                 />
                             </div>
                             )
-
                     }
                 </div>
             </div>
